ddfc006acabef328f6128aeff904f3b227e1824e
danilowhk <danilowhk@gmail.com>
2022-10-09 07:30:28 -0300

    build fix

--- contracts/src/dist/types/index.d.ts
+++
@@ -1,5 +0,0 @@
-export type { MyWallet } from "./MyWallet";
-export type { MyWalletDeployer } from "./MyWalletDeployer";
-export * as factories from "./factories";
-export { MyWallet__factory } from "./factories/MyWallet__factory";
-export { MyWalletDeployer__factory } from "./factories/MyWalletDeployer__factory";
