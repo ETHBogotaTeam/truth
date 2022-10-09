12f782f3108e52c8991f0d02ead8e731285f861c
danilowhk <danilowhk@gmail.com>
2022-10-09 03:53:29 -0300

    Ui

--- pages/index.js
+++ pages/index.js
@@ -155,7 +155,7 @@ export default function Home() {
     }
 
     return (
-        <div className="p-2">
+        <div className="">
             <Publish
                 publishIsOpen={publishIsOpen}
                 closePublishModal={closePublishModal}
@@ -173,67 +173,136 @@ export default function Home() {
                 />
             </Head>
 
-            <main className="bg-[url('/bg.jpg')] bg-opacity-20 p-0 m-0 bg-cover bg-no-repeat bg-center bg-fixed">
-                <div className="flex">
-                    <span className="text-gray-500 font-medium">Truth.</span>{" "}
-                    <Logo />
-                </div>
-                <h2 className="flex justify-center font-bold text-lg">
-                    Explore
-                </h2>
-                <button
+            <main>  
+            <div className="flex flex-col r">
+            {/* <div className="bg-[url('/bg.jpg')] opacity-30 p-0 m-0 bg-cover bg-no-repeat bg-center bg-fixed h-[900px]"></div> */}
+
+                <div className="z-10 p-3 flex items-center justify-between bg-gray-100">
+                
+                    <div className="flex">
+                        <span className="text-gray-500 font-medium">Truth.</span>{" "}
+                        <Logo />
+                    </div>
+
+                    <button
                     type="button"
-                    className="border-2 p-4"
+                    className="border-2 rounded-md p-4"
                     onClick={connect}
                 >
                     Connect
                 </button>
+
+                </div>
+                <div className="flex items-center justify-center p-3">
+                    <h2 className="flex  font-bold text-lg">
+                        Explore
+                    </h2>
+                </div>
+                <div className="flex flex-col items-center justify-center p-3 mb-3">
+                    <div className="flex flex-col font-bold text-lg w-[400px] h-[480px] bg-gray-300 rounded-xl px-2">
+                        <image className="bg-gray-700 h-[70%] w-full relative">Image 1
+                            <div className="bg-white opacity-50 absolute bottom-0 w-full h-auto flex flex-col items-center justify-center py-2">
+                                <div className="bg-gray-800 rounded-xl text-white px-2">Verified by Truth</div>
+                                <div className="flex justify-between w-full px-3">
+                                    <div>Location</div>
+                                    <div>Date</div>
+                                </div>
+
+                            </div>  
+                        </image>
+                        <div className="h-[30%] flex items-center justify-between px-5">
+                            <div className="bg-gray-500 h-[70%] w-[30%] rounded-md">
+                                Extra 1
+                            </div>
+                            <div className="bg-gray-500 h-[70%] w-[30%] rounded-md">
+                                Extra 1
+                            </div>
+                            <div className="bg-gray-500 h-[70%] w-[30%] rounded-md">
+                                Extra 1
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col font-bold text-lg w-[400px] h-[380px] bg-gray-300 rounded-xl px-2">
+                        <image className="bg-gray-700 h-[70%] w-full relative">Image 1
+                            <div className="bg-white opacity-50 absolute bottom-0 w-full h-[50px] flex items-center justify-center">
+                                <div className="bg-white rounded-xl">Verified by Truth</div>
+                            </div>  
+                        </image>
+                        <div className="h-[30%] flex items-center justify-between px-5">
+                            <div className="bg-gray-500 h-[70%] w-[30%] rounded-md">
+                                Extra 1
+                            </div>
+                            <div className="bg-gray-500 h-[70%] w-[30%] rounded-md">
+                                Extra 1
+                            </div>
+                            <div className="bg-gray-500 h-[70%] w-[30%] rounded-md">
+                                Extra 1
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className="flex flex-col font-bold text-lg w-[400px] h-[380px] bg-gray-300 rounded-xl px-2">
+                        <image className="bg-gray-700 h-[70%] w-full relative">Image 1
+                            <div className="bg-white opacity-50 absolute bottom-0 w-full h-[50px] flex items-center justify-center">
+                                <div className="bg-white rounded-xl">Verified by Truth</div>
+                            </div>  
+                        </image>
+                        <div className="h-[30%] flex items-center justify-between px-5">
+                            <div className="bg-gray-500 h-[70%] w-[30%] rounded-md">
+                                Extra 1
+                            </div>
+                            <div className="bg-gray-500 h-[70%] w-[30%] rounded-md">
+                                Extra 1
+                            </div>
+                            <div className="bg-gray-500 h-[70%] w-[30%] rounded-md">
+                                Extra 1
+                            </div>
+                        </div>
+                    </div>
+
+   
+                </div>
+
+                </div>
+{/* 
                 <button
                     type="button"
                     className="border-2 p-4"
                     onClick={() => setPublishIsOpen(true)}
                 >
                     Publish
-                </button>
-                <h1>Truth</h1>
-                {posts.map((post, index) => (
-                    <a
-                        href={`/profile/${
-                            post.profile.id || post.profile.profileId
-                        }`}
-                        key={index}
-                    >
-                        <a>
-                            <div>
-                                {/* <p>{typeMap[post.__typename]}</p> */}
-                                <div>
-                                    {post.profile.picture &&
-                                    post.profile.picture.original ? (
-                                        <img
-                                            src={
-                                                post.profile.picture.original
-                                                    .url
-                                            }
-                                        />
-                                    ) : (
-                                        <div />
-                                    )}
+                </button> */}
+                {/* <h1>Truth</h1> */}
+                <div className="flex flex-col items-center justify-center">
 
-                                    <div>
-                                        <h3>{post.profile.name}</h3>
-                                        <p>{post.profile.handle}</p>
-                                    </div>
-                                </div>
-                                <div>
-                                    <p>{post.metadata.content}</p>
-                                </div>
-                            </div>
-                        </a>
-                    </a>
-                ))}
+
+                </div>
+
+                <div className="fixed bottom-0 h-[100px] flex items-center justify-between w-full bg-[#324E7B] flex px-3 ">
+                    <svg width="18" height="20" viewBox="0 0 18 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M18 8.15033V15.9668C18 18.1943 16.2091 20 14 20H4C1.79086 20 0 18.1943 0 15.9668V8.15033C0 6.93937 0.539645 5.7925 1.46986 5.02652L6.46986 0.909348C7.9423 -0.303114 10.0577 -0.303117 11.5301 0.909345L16.5301 5.02652C17.4604 5.7925 18 6.93937 18 8.15033ZM12.25 15.25V17.5C12.25 18.0523 11.8023 18.5 11.25 18.5H6.75C6.19772 18.5 5.75 18.0523 5.75 17.5V15.25C5.75 13.4551 7.20507 12 9 12C10.7949 12 12.25 13.4551 12.25 15.25Z" fill="white"/>
+                    </svg>
+
+                    <svg width="21" height="18" viewBox="0 0 21 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M8.32309 0H9.46595C12.7013 0 15.4133 2.28735 16.1349 5.36434C16.2579 5.88907 16.3231 6.43677 16.3231 7C16.3231 10.866 13.253 14 9.46595 14H5.2847H2.6088C1.34644 14 0.32309 12.9553 0.32309 11.6667V8.16667C0.32309 3.65634 3.90481 0 8.32309 0ZM9.75166 16H6.38123C7.618 17.2372 9.31185 18 11.1802 18H15.3615H18.0374C19.2997 18 20.3231 16.9553 20.3231 15.6667V12.1667C20.3231 10.0939 19.5666 8.20142 18.3201 6.76135C18.3221 6.84065 18.3231 6.9202 18.3231 6.99999C18.3231 11.9706 14.4855 16 9.75166 16Z" fill="white"/>
+                    </svg>
+
+
+                    <p className="bg-white rounded-full w-[81px] h-[81px] flex items-center justify-center">+</p>
+                    <svg width="18" height="20" viewBox="0 0 18 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M18 8.15033V15.9668C18 18.1943 16.2091 20 14 20H4C1.79086 20 0 18.1943 0 15.9668V8.15033C0 6.93937 0.539645 5.7925 1.46986 5.02652L6.46986 0.909348C7.9423 -0.303114 10.0577 -0.303117 11.5301 0.909345L16.5301 5.02652C17.4604 5.7925 18 6.93937 18 8.15033ZM12.25 15.25V17.5C12.25 18.0523 11.8023 18.5 11.25 18.5H6.75C6.19772 18.5 5.75 18.0523 5.75 17.5V15.25C5.75 13.4551 7.20507 12 9 12C10.7949 12 12.25 13.4551 12.25 15.25Z" fill="white"/>
+                    </svg>
+
+                    <svg width="21" height="18" viewBox="0 0 21 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M8.32309 0H9.46595C12.7013 0 15.4133 2.28735 16.1349 5.36434C16.2579 5.88907 16.3231 6.43677 16.3231 7C16.3231 10.866 13.253 14 9.46595 14H5.2847H2.6088C1.34644 14 0.32309 12.9553 0.32309 11.6667V8.16667C0.32309 3.65634 3.90481 0 8.32309 0ZM9.75166 16H6.38123C7.618 17.2372 9.31185 18 11.1802 18H15.3615H18.0374C19.2997 18 20.3231 16.9553 20.3231 15.6667V12.1667C20.3231 10.0939 19.5666 8.20142 18.3201 6.76135C18.3221 6.84065 18.3231 6.9202 18.3231 6.99999C18.3231 11.9706 14.4855 16 9.75166 16Z" fill="white"/>
+                    </svg>
+                    
+                </div>
             </main>
 
-            <footer>Powered by Truth</footer>
+            <footer className="h-[70px]">Powered by Truth</footer>
         </div>
     )
 }
