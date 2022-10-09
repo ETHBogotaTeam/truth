f511be78b52ea13401d17999b985312827950eb5
Rich Warner <richwarneriii@gmail.com>
2022-10-09 05:55:55 -0600

    stuff

--- components/PublishModal.js
+++ components/PublishModal.js
@@ -1,5 +1,6 @@
 import { Dialog } from "@headlessui/react"
 import { useState, Fragment, useEffect, useRef } from "react"
+import Map from "./Map.js"
 const { Web3Storage, File } = require("web3.storage")
 const { Blob } = require("@web-std/blob")
 
@@ -10,6 +11,9 @@ const PublishModal = ({
     publishPost,
     setPublishIsOpen
 }) => {
+    const [screen, setScreen] = useState(1)
+    const [photoUri, setPhotoUri] = useState("")
+
     const onPhotoChange = (event) => {
         const web3StorageApiToken =
             "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGRjMmZGNUI1OTk4MTkwMDc4NTVkMWRGRkQ4MDI5YTExMDk5Q2JBOTUiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NjI0NTYyNTkwNjgsIm5hbWUiOiJUQVoifQ.QAR-mcLPEPtmDU4Mod8olwCV_2_T4O5PQU-r-KkRe0A" // process.env.WEB3_STORAGE_API_TOKEN
@@ -22,7 +26,7 @@ const PublishModal = ({
             .put([file], { wrapWithDirectory: false })
             .then((dataCid) => {
                 const imagelUri = "https://" + dataCid + ".ipfs.dweb.link"
-                alert("Uri for serving image: " + imagelUri)
+                setPhotoUri(imagelUri)
             })
     }
 
@@ -41,30 +45,55 @@ const PublishModal = ({
                 <div className="flex min-h-full items-center justify-center p-4">
                     {/* The actual dialog panel  */}
                     <Dialog.Panel className="mx-auto max-w-sm rounded bg-white">
-                        <Dialog.Title className="border-b-2 p-6 text-center font-semibold text-xl tracking-tighter bg-slate-100">
-                            Publish a Photo
-                        </Dialog.Title>
+                        {
+                            {
+                                1: (
+                                    <div>
+                                        <Dialog.Title className="border-b-2 p-6 text-center font-semibold text-xl tracking-tighter bg-slate-100">
+                                            Publish a Photo
+                                        </Dialog.Title>
 
-                        <div className="p-8">
-                            <div className="mb-4">
-                                <input
-                                    type="file"
-                                    accept="image/*"
-                                    capture="capture"
-                                    onChange={onPhotoChange}
-                                    className="border-2 p-4"
-                                ></input>
-                            </div>
-                            <div className="flex justify-end">
-                                <button
-                                    type="button"
-                                    className="flex border-2 px-6 py-2 rounded-lg bg-[#324E7B] text-white"
-                                    onChange=""
-                                >
-                                    Next
-                                </button>
-                            </div>
-                        </div>
+                                        <div className="p-8">
+                                            <div className="mb-4">
+                                                <input
+                                                    type="file"
+                                                    accept="image/*"
+                                                    capture="capture"
+                                                    onChange={onPhotoChange}
+                                                    className="border-2 p-4"
+                                                ></input>
+                                            </div>
+                                            {photoUri && (
+                                                <div className="mb-4 border-2 p-4 text-ellipsis truncate">
+                                                    {photoUri}
+                                                </div>
+                                            )}
+                                            <div className="flex justify-end">
+                                                <button
+                                                    type="button"
+                                                    className="flex border-2 px-6 py-2 rounded-lg bg-[#324E7B] text-white"
+                                                    onClick={() => {
+                                                        setScreen(2)
+                                                    }}
+                                                >
+                                                    Next
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ),
+                                2: (
+                                    <div>
+                                        <Dialog.Title className="border-b-2 p-6 text-center font-semibold text-xl tracking-tighter bg-slate-100">
+                                            Select Your Location
+                                        </Dialog.Title>
+                                        <div>
+                                            <Map />
+                                        </div>
+                                    </div>
+                                )
+                            }[screen]
+                        }
                     </Dialog.Panel>
                 </div>
             </div>
--- components/map.js
+++ components/map.js
@@ -1,279 +1,262 @@
-import React from 'react'
-import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
-import {useState} from 'react'
-const snarkjs = require('snarkjs');
-import Router from 'next/router'
-
+import React from "react"
+import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api"
+import { useState } from "react"
+//import Router from 'next/router'
+const snarkjs = require("snarkjs")
 
 const statsLat = [
-  { name: 'Min Longitude', stat: 100},
-  { name: 'Max Longitude', stat: 150 },
+    { name: "Min Longitude", stat: 100 },
+    { name: "Max Longitude", stat: 150 }
 ]
 
 const statsLong = [
-  { name: 'Total Subscribers', stat: '71,897' },
-  { name: 'Avg. Open Rate', stat: '58.16%' },
+    { name: "Total Subscribers", stat: "71,897" },
+    { name: "Avg. Open Rate", stat: "58.16%" }
 ]
 
 const statsCurrent = [
-  { name: 'Total Subscribers', stat: '71,897' },
-  { name: 'Avg. Open Rate', stat: '58.16%' },
+    { name: "Total Subscribers", stat: "71,897" },
+    { name: "Avg. Open Rate", stat: "58.16%" }
 ]
 
 const containerStyle = {
-  width: '1100px',
-  height: '700px'
-};
+    width: "380px",
+    height: "440px"
+}
 
 var center = {
-  lat: 4.62,
-  lng: -74.09
-};
+    lat: 4.62,
+    lng: -74.09
+}
 
+export default function Map() {
+    const [isConnected, setIsConnected] = useState()
 
-function MyComponent() {
+    const [minLat, setMinLat] = useState(0)
+    const [maxLat, setMaxLat] = useState(0)
+    const [minLong, setMinLong] = useState(0)
+    const [maxLong, setMaxLong] = useState(0)
+    const [currentLat, setCurrentLat] = useState(0)
+    const [currentLong, setCurrentLong] = useState(0)
+    const [mainProof, setMainProof] = useState()
+    const [mainPublicSignals, setMainPublicSignals] = useState()
+    const [isVerified, setIsVerified] = useState(false)
 
-  const [isConnected, setIsConnected] = useState();
+    const [activeMarker, setActiveMarker] = useState(null)
 
-
-  const [minLat,setMinLat] = useState(0);
-  const [maxLat,setMaxLat] = useState(0);
-  const [minLong,setMinLong] = useState(0);
-  const [maxLong,setMaxLong] = useState(0);
-  const [currentLat,setCurrentLat] = useState(0);  
-  const [currentLong,setCurrentLong] = useState(0);  
-  const [mainProof ,setMainProof] = useState();
-  const [mainPublicSignals,setMainPublicSignals] = useState();
-  const [isVerified,setIsVerified] = useState(false);
-
-  const [activeMarker, setActiveMarker] = useState(null);
-
-  const handleActiveMarker = (marker) => {
-    if (marker === activeMarker) {
-      return;
+    const handleActiveMarker = (marker) => {
+        if (marker === activeMarker) {
+            return
+        }
+        setActiveMarker(marker)
     }
-    setActiveMarker(marker);
-  };
 
-  function sendProps(){
-    Router.push({
-        pathname:"/select-nft",
-        query:{
-            minLat,
-            maxLat,
-            minLong,
-            maxLong,
-            currentLat,
-            currentLong
+    // function sendProps(){
+    //   Router.push({
+    //       pathname:"/select-nft",
+    //       query:{
+    //           minLat,
+    //           maxLat,
+    //           minLong,
+    //           maxLong,
+    //           currentLat,
+    //           currentLong
+    //       }
+
+    //   })
+    // }
+
+    const NFT_1 = [
+        //     {
+        //       id: 1,
+        //       timestamp: "20220719022435",
+        //       position: { lat: 48.84, lng:2.35 }
+        //     },
+        //     {
+        //       id: 2,
+        //       timestamp: "20220719022435",
+        //       position: { lat: 48.85, lng: 2.33 }
+        //     },
+        //     {
+        //       id: 3,
+        //       timestamp: "20220719022435",
+        //       position: { lat: 48.8427, lng: 2.345 }
+        //     },
+        //     {
+        //       id: 4,
+        //       timestamp: "20220719022435",
+        //       position: { lat: 48.8531, lng: 2.34 }
+        //     }
+    ]
+
+    const { isLoaded } = useJsApiLoader({
+        id: "google-map-script",
+        googleMapsApiKey: "AIzaSyAIJx9hwjxcmt2-VjyW5MCxwPqlVvTW51w"
+    })
+
+    const [map, setMap] = React.useState(null)
+
+    const onLoad = React.useCallback(function callback(map) {
+        const bounds = new window.google.maps.LatLngBounds(center)
+        map.fitBounds(bounds)
+        setMap(map)
+    }, [])
+
+    const onUnmount = React.useCallback(function callback(map) {
+        setMap(null)
+    }, [])
+
+    async function getFileBuffer(filename) {
+        let req = await fetch(filename)
+        return Buffer.from(await req.arrayBuffer())
+    }
+
+    async function generateProof(state, setState) {
+        // Load files and run proof locally
+        let DOMAIN = "http://localhost:3000"
+        let VERCEL_DOMAIN = "https://daordinate-three.vercel.app/"
+        let wasmBuff = await getFileBuffer(`${DOMAIN}/inRange.wasm`)
+        let zkeyBuff = await getFileBuffer(`${DOMAIN}/inRange.zkey`)
+
+        let input = {
+            latitudeRange: [parseInt(minLat) + 1000, parseInt(maxLat) + 1000],
+            longitudeRange: [
+                parseInt(minLong) + 1000,
+                parseInt(maxLong) + 1000
+            ],
+            currentLocation: [
+                parseInt(currentLat) + 1000,
+                parseInt(currentLong) + 1000
+            ]
         }
 
+        try {
+            let { proof, publicSignals } = await snarkjs.plonk.fullProve(
+                input,
+                wasmBuff,
+                zkeyBuff
+            )
+
+            setMainProof(proof)
+            setMainPublicSignals(publicSignals)
+
+            //   setState({...state, proof:proof, publicSignals:publicSignals})
+        } catch (error) {
+            alert("Location too far away: Invalid Proof")
+        }
+    }
+
+    async function verifyProof(state, setState) {
+        let vkey = await fetch(
+            `localhost:3000/inRange_verification_key.json`
+        ).then((res) => res.json())
+
+        const verified = await snarkjs.plonk.verify(
+            vkey,
+            mainPublicSignals,
+            mainProof
+        )
+
+        setIsVerified(verified)
+    }
+
+    const [location, setLocation] = useState({
+        lat: "",
+        long: ""
     })
-  }
 
-  const NFT_1 = [
-    
-//     {
-//       id: 1,
-//       timestamp: "20220719022435",
-//       position: { lat: 48.84, lng:2.35 }
-//     },
-//     {
-//       id: 2,
-//       timestamp: "20220719022435",
-//       position: { lat: 48.85, lng: 2.33 }
-//     },
-//     {
-//       id: 3,
-//       timestamp: "20220719022435",
-//       position: { lat: 48.8427, lng: 2.345 }
-//     },
-//     {
-//       id: 4,
-//       timestamp: "20220719022435",
-//       position: { lat: 48.8531, lng: 2.34 }
-//     }
-    
-  ];
-  
-  
+    function getLocation() {
+        const onSuccess = (loc) => {
+            setLocation({
+                lat: loc.coords.latitude,
+                long: loc.coords.longitude
+            })
 
-  const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: "AIzaSyAIJx9hwjxcmt2-VjyW5MCxwPqlVvTW51w"
-  })
+            const lat = loc.coords.latitude
+            const long = loc.coords.longitude
+            setCurrentLat(lat)
+            setCurrentLong(long)
+        }
 
-  const [map, setMap] = React.useState(null)
+        const onError = () => {
+            console.log("Could not get Location")
+        }
 
-  const onLoad = React.useCallback(function callback(map) {
-    const bounds = new window.google.maps.LatLngBounds(center);
-    map.fitBounds(bounds);
-    setMap(map)
-  }, [])
-
-  const onUnmount = React.useCallback(function callback(map) {
-    setMap(null)
-  }, [])
-
-    
-  async function getFileBuffer(filename) {
-      let req = await fetch(filename);
-      return Buffer.from(await req.arrayBuffer());
-  }
-
-  async function generateProof(state, setState) {
-
-      // Load files and run proof locally
-      let DOMAIN = "http://localhost:3000";
-      let VERCEL_DOMAIN ="https://daordinate-three.vercel.app/";
-      let wasmBuff = await getFileBuffer(`${DOMAIN}/inRange.wasm`);
-      let zkeyBuff = await getFileBuffer(`${DOMAIN}/inRange.zkey`);
-    
-      let input = {
-          "latitudeRange": [ parseInt(minLat) + 1000, parseInt(maxLat) + 1000],
-          "longitudeRange": [ parseInt(minLong) + 1000, parseInt(maxLong) + 1000],
-          "currentLocation": [ parseInt(currentLat) + 1000, parseInt(currentLong) + 1000]
-      }
-      
-    
-      try {
-    
-        let {proof, publicSignals} = await snarkjs.plonk.fullProve(input, wasmBuff, zkeyBuff);
-        
-        setMainProof(proof);
-        setMainPublicSignals(publicSignals);
-
-        
-      //   setState({...state, proof:proof, publicSignals:publicSignals})
-    
-      } catch (error) {
-        alert("Location too far away: Invalid Proof")
-      } 
+        navigator.geolocation.getCurrentPosition(onSuccess, onError)
     }
 
+    function getLatTarget() {
+        const onSuccess = (loc) => {
+            setMinLat(loc - 10)
+            setMaxLat(loc + 10)
+            console.log("Target")
+            console.log(minLat)
+            console.log(maxLat)
+            console.log(loc)
+        }
 
-  async function verifyProof(state , setState ) {
-
-      let vkey = await fetch (`localhost:3000/inRange_verification_key.json`).then(res => res.json());
-    
-      const verified = await snarkjs.plonk.verify(vkey, mainPublicSignals, mainProof);
-    
-      setIsVerified(verified)
-    
-    }
-    
-
-
-  const [location,setLocation] = useState({
-      lat:"",
-      long:"",
-  })
-
-  function getLocation(){
-      const onSuccess = (loc) => {
-          setLocation({
-              lat: loc.coords.latitude,
-              long: loc.coords.longitude
-          })
-         
-    
-
-          const lat =loc.coords.latitude;
-          const long = loc.coords.longitude;
-          setCurrentLat(lat)
-          setCurrentLong(long)
-
-
-      }
-
-      const onError =()=>{
-          console.log("Could not get Location")
-      }
-
-      navigator.geolocation.getCurrentPosition(onSuccess,onError)
-  }
-
-  function getLatTarget(){
-    const onSuccess = (loc) => {
-        setMinLat(loc-10)
-        setMaxLat(loc+10)
-        console.log("Target")
-        console.log(minLat)
-        console.log(maxLat)
-        console.log(loc)
+        const onError = () => {
+            console.log("Could not get Location")
+        }
     }
 
-    const onError =()=>{
-        console.log("Could not get Location")
-    }
-  }
-
-  return (
-  <div className="flex flex-col items-center justify-evenly">
-
-    {isLoaded ? (
-    
-      <GoogleMap
-      onLoad={onLoad}
-      onUnmount={onUnmount}
-        onClick={ev => {
-          setMinLat(ev.latLng.lat());
-          setMaxLat(ev.latLng.lat()+100);
-          setMinLong(ev.latLng.lng());
-          setMaxLong(ev.latLng.lng()+100);
-
-          
-          () => setActiveMarker(null)
-          getLatTarget}}
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={15}
-      >
-      </GoogleMap>
-    ):<></>}
-  <div className="ml-2 mr-2">
-
-
-      <button
-            type="button"
-            className="w-full mt-5 items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            onClick={getLocation}
-        >
-        Get Location
-      </button>
-      <div className="mt-5 grid grid-cols-2 gap-1 sm:grid-cols-2">
-        <button
-              type="button"
-              className="ml-1 mr-1 w-full text-center px-4 py-2 border border-transparent font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              onClick={generateProof}
-          >
-          Generate Proof
-        </button>
-        <button
-              type="button"
-              className="ml-1 mr-2 w-full text-center px-4 py-2 border border-transparent font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              onClick={verifyProof}
-          >
-          Verify Proof
-        </button>
-      
-      </div>
-      {
-        isVerified ? 
+    return (
+        <div className="flex flex-col justify-evenly">
+            {isLoaded ? (
+                <div className="w-[400px]">
+                    <GoogleMap
+                        onLoad={onLoad}
+                        onUnmount={onUnmount}
+                        onClick={(ev) => {
+                            setMinLat(ev.latLng.lat())
+                            setMaxLat(ev.latLng.lat() + 100)
+                            setMinLong(ev.latLng.lng())
+                            setMaxLong(ev.latLng.lng() + 100)
+                            ;() => setActiveMarker(null)
+                            getLatTarget
+                        }}
+                        mapContainerStyle={containerStyle}
+                        center={center}
+                        zoom={11}
+                    ></GoogleMap>
+                </div>
+            ) : (
+                <></>
+            )}
+            <div className="ml-2 mr-2 mb-4">
+                <button
+                    type="button"
+                    className="w-full mt-5 items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    onClick={getLocation}
+                >
+                    Get Location
+                </button>
+                <div className="mt-5 grid grid-cols-2 gap-1 sm:grid-cols-2">
+                    <button
+                        type="button"
+                        className="ml-1 mr-1 w-full text-center px-4 py-2 border border-transparent font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        onClick={generateProof}
+                    >
+                        Generate Proof
+                    </button>
+                    <button
+                        type="button"
+                        className="ml-1 mr-2 w-full text-center px-4 py-2 border border-transparent font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        onClick={verifyProof}
+                    >
+                        Verify Proof
+                    </button>
+                </div>
+                {isVerified ? (
                     <button
                         type="button"
                         className="w-full mt-5 text-center items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                         onClick={sendProps}
                     >
-                    Next
-                    </button> : null     
-        }
-  </div>
-
-  
- 
-
-
-  </div>)
+                        Next
+                    </button>
+                ) : null}
+            </div>
+        </div>
+    )
 }
-
-export default React.memo(MyComponent)
